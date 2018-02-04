4d682c72a10fb6a2882de768d35d872fa88a4803
hpiyankov <hpiyankov@gmail.com>
2018-02-04 08:51:09 +0200

    Synced the hasing logic with the Block.js file from the node

--- SimpleStupidMiner.py
+++ SimpleStupidMiner.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[7]:
+# In[2]:
 
 
 import hashlib
@@ -9,13 +9,15 @@ import requests
 from threading import Timer
 
 nonce = 0
+user = "Hristo"
 nodeURL = "3333333"
 getTask = "bbbb"
 givePoW = "cccc"
 
 #Dummy data for now
 node_data = {
-    'blockHash': 'Default hash before data fetch',
+    'hashForMiner': 'Default hash before data fetch',
+    'timestamp': 'SomeDate',
     'difficulty' : 6,
 }
 
@@ -27,14 +29,16 @@ def checkForUpdates():
     
     global blockHash
     global difficulty
+    global timestamp
     
     #Get the previous hash and the difficulty form the Node
     #response = requests.get(nodeURL+getTask)
     #node_data = response.json()
-    blockHash = node_data["blockHash"]
+    blockHash = node_data["hashForMiner"]
     difficulty = node_data["difficulty"]
+    timestamp = node_data["timestamp"]
     
-    print(f'Running for block hash:\033[92m {blockHash}\033[0m on difficulty\033[91m {difficulty}\033[0m at cumulative iteration \033[4m{nonce}\033[0m')
+    print(f'Running for block hash:\033[92m {blockHash}\033[0m on difficulty\033[91m {difficulty}\033[0m on stamp \033[92m{timestamp}\033[0m at cumulative iteration \033[4m{nonce}\033[0m')
     return
 
 def hash_256(string):
@@ -44,7 +48,7 @@ def solutionFound(result):
     #Push the solution to the Node
     print(f'\033[94mSolution found for\033[1m nonce = {nonce} hash = {result}\033[91m ')
     
-    data = {'nonce':nonce,'Proof-of-work':result}
+    data = {'nounce':nonce,'Proof-of-work':result, 'minedBy':user, "timestamp":timestamp}
     #requests.put(nodeURL+givePoW, data=data)
     return
 
@@ -52,7 +56,7 @@ checkForUpdates()
 
 
 for nonce in range(2**1000):
-    result = hash_256(blockHash+str(nonce))
+    result = hash_256(blockHash+user+str(timestamp)+str(nonce))
     if result[:difficulty] == '0'*difficulty:
         solutionFound(result)
   
